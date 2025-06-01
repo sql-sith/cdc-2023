1d39002152a547af18a4da426ba1752b8b011b3b
Chris Leonard <chris@databaseguy.com>
2025-06-01 00:47:52 -0500

    committing old branch found 1-june-2025

--- summer session/sql-server/02-run-arbitrary-queries.py
+++ summer session/sql-server/02-run-arbitrary-queries.py
@@ -1,4 +1,5 @@
-from util import *
+import pyodbc
+
 
 DRIVER = "ODBC Driver 18 for SQL Server"
 SERVER = "Caerdydd"
@@ -25,6 +26,53 @@ SEMI_CRAZY_SALES_QUERY = " \
     CROSS APPLY s.Demographics.nodes('(awss:StoreSurvey)[1]') AS store(x) \
     ORDER BY Store \
     "
+
+
+def display_query_results(cursor: pyodbc.Cursor):
+    print('\n', end='')
+
+    coldefs = cursor.description
+    colmeta = {}
+    for colidx in range(len(coldefs)):
+        col = coldefs[colidx]
+        colmeta[colidx] = {
+            "name": col[0],
+            "width": col[4] if col[4] > len(col[0]) else len(col[0])
+            }
+
+    for id in colmeta.keys():
+        meta = colmeta[id]
+        print(f'{meta["name"]:<{meta["width"]}} ', end='')
+    print('\n', end='')
+
+    for id in colmeta.keys():
+        meta = colmeta[id]
+        print("-" * meta["width"] + ' ', end='')
+    print('\n', end='')
+
+    for row in cursor:
+        for id in colmeta.keys():
+            print(f"{str(row[id]):<{colmeta[id]['width']}} ", end='')
+        print('\n', end='')
+
+    print('\n', end='')
+
+
+def get_connection(driver: str, server: str, database: str) -> pyodbc.Connection:
+    return pyodbc.connect(
+        f"Driver={{{driver}}};"
+        f"Server={server};"
+        f"Database={database};"
+        "Trusted_Connection=yes;"
+        "TrustServerCertificate=yes;"
+        "Encrypt=yes"
+        )
+
+
+def execute_query(connection: pyodbc.Connection, query: str):
+    cursor = connection.cursor().execute(query)
+    display_query_results(cursor)
+    cursor.close()
 
 
 if __name__ == '__main__':
